ab7f59a65dc029b1c6c598d89179d92776691447
kavsik-krish <152492372+kavsik-krish@users.noreply.github.com>
2024-06-17 13:54:37 +0530

    commit made by backend api

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
 
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = "keys.json"
+  project     = "xfbb"
+  region      = "fsbfsb"
+}
+
+resource "google_pubsub_topic" "szdv" {
+  name     = "szdv"
+  location = "us-central1-c"
+}
+
+resource "google_pubsub_subscription" "sdbbf" {
+  name          = "sdbbf"
+  topic         = google_pubsub_topic.szdv.id
+  ack_deadline_seconds = 56
+  push_config {
+    push_endpoint = "fbnb"
+  }
+  location = "us-central1-c"
+}
