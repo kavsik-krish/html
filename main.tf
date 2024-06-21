958f8899e610c725f498253f74e6baf0001af281
kavsik-krish <152492372+kavsik-krish@users.noreply.github.com>
2024-06-21 12:38:36 +0530

    commit made by backend api

--- main.tf
+++ main.tf
@@ -1,23 +1,14 @@
 
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
+resource "google_cloud_run_v2_service" "default" {
+  autogenerate_revision_name = true
+  location                  = "dkjn"
+  metadata {
+    name = "kjsbk"
+  }
+  project = "macndn"
+  template {
+    containers {
+      image = "kjankj"
     }
   }
-}
-
-provider "google" {
-  credentials = file("keys.json")
-  project     = "acbjkj"
-}
-
-resource "google_cloudfunctions_function" "default" {
-  name     = "djksbckj"
-  runtime  = "kjsbkj"
-  entry_point = "kjsbck"
-  source_archive_bucket = "skjbk"
-  trigger_http = true
-  region = "mnax"
 }
