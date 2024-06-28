d573638e13a066db3c1b78f8aa6fa171e8cce699
terraAI <terraAI@example.com>
2024-06-28 13:39:55 +0530

    Commit made by the backend process

--- main.tf
+++ main.tf
@@ -16,53 +16,16 @@ provider "google" {
 resource "google_pubsub_topic" "default" {
   name     = "pubsub-topic-basic"
   project  = "abc"
-  location = "us-central1"
-}
-
-resource "google_cloudfunctions_function" "default" {
-  name     = "cloudfunctions-function-basic"
-  runtime  = "nodejs16"
-  entry_point = "helloHTTP"
-  source_archive_bucket = "gs://cloudfunctions-source-bucket-basic"
-  source_archive_object = "function.zip"
-  trigger_http = true
-  project  = "abc"
-  region   = "us-central1"
-}
-
-resource "google_cloud_run_v2_service" "default" {
-  name     = "cloudrun-service-basic"
-  location = "us-central1"
-  template {
-    containers {
-      image = "us-docker.pkg.dev/cloudrun/container/hello"
-    }
+  labels = {
+    environment = "dev"
   }
-  project = "abc"
 }
 
-resource "google_dataflow_job" "default" {
-  name     = "dataflow-job-basic"
-  location = "us-central1"
+resource "google_pubsub_subscription" "default" {
+  name     = "pubsub-subscription-basic"
+  topic    = google_pubsub_topic.default.id
   project  = "abc"
-  template {
-    launch_template {
-      job_name = "dataflow-job-basic"
-    }
-    environment {
-      temp_bucket = "gs://dataflow-temp-bucket-basic"
-    }
-    transform {
-      outer_input {
-        bigquery_source {
-          table = "project:dataset.table"
-        }
-      }
-      outer_output {
-        bigquery_sink {
-          table = "project:dataset.table"
-        }
-      }
-    }
+  labels = {
+    environment = "dev"
   }
 }
