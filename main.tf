e52411bda5dd83f81d9643415bb32a005ac5c966
terraAI <terraAI@example.com>
2024-07-17 05:44:32 +0000

    Commit made by the backend process

--- main.tf
+++ main.tf
@@ -1,12 +1,43 @@
 
-resource "google_cloud_run_v2_service" "default" {
-  name     = "ncdh"
-  location = "asia-northeast1-b"
-  project  = "lumen-fir-w1"
-
-  template {
-    containers {
-      image = "urlimage"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
     }
+  }
+}
+
+provider "google" {
+  credentials = file("keys.json")
+  project     = "abc"
+}
+
+resource "google_container_cluster" "default" {
+  name     = "gcp-kubernetes-cluster"
+  location = "us-central1"
+  initial_node_count = 3
+  node_config {
+    machine_type = "e2-medium"
+  }
+  master_auth {
+    username = "admin"
+  }
+  network = "default"
+  subnetwork = "projects/gcp-project-id/regions/us-central1/subnetworks/default"
+}
+
+resource "google_sql_database" "default" {
+  name     = "gcp-sql-database"
+  instance = "gcp-sql-instance"
+  charset  = "UTF8"
+}
+
+resource "google_sql_database_instance" "default" {
+  name             = "gcp-sql-instance"
+  database_version = "MYSQL_5_7"
+  region           = "us-central1"
+  settings {
+    tier = "db-f1-micro"
   }
 }
